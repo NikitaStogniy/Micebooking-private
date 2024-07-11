ac50f509929014dfb82fd39e6478e19545b3a147
vldslvvvl <vldslvvvlv@gmail.com>
2024-07-11 19:52:15 +0300

    add fixed files

---
+++ lib/client/client_desires/client_desires_widget.dart
@@ -0,0 +1,53 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'client_desires_model.dart';
+export 'client_desires_model.dart';
+
+class ClientDesiresWidget extends StatefulWidget {
+  const ClientDesiresWidget({
+    super.key,
+    required this.services,
+  });
+
+  final List<int>? services;
+
+  @override
+  State<ClientDesiresWidget> createState() => _ClientDesiresWidgetState();
+}
+
+class _ClientDesiresWidgetState extends State<ClientDesiresWidget> {
+  late ClientDesiresModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => ClientDesiresModel());
+
+    // On component load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      _model.clientServices = widget.services!.toList().cast<int>();
+      setState(() {});
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
