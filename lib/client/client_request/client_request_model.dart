1882846ab6bad3dfee665dd94410df0c100b93ce
vldslvvvl <vldslvvvlv@gmail.com>
2024-07-11 19:46:27 +0300

    client request error fix

---
+++ lib/client/client_request/client_request_model.dart
@@ -0,0 +1,21 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'client_request_widget.dart' show ClientRequestWidget;
+import 'package:flutter/material.dart';
+
+class ClientRequestModel extends FlutterFlowModel<ClientRequestWidget> {
+  ///  Local state fields for this component.
+
+  bool requestOpen = false;
+
+  int? request;
+
+  DateTime? currentDate;
+
+  String? formatedDate;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
